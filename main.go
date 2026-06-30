93302bad1824d97a1e6dfd950f91babf84849941
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-06-30 23:09:05 +0300

    feat: add function downloadFile

--- main.go
+++ main.go
@@ -2,8 +2,48 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path"
 )
+
+func downloadFile(url, savePaths string) error {
+	if err := os.MkdirAll(savePaths, os.ModePerm); err != nil {
+		return fmt.Errorf("не удалось создать директорию: %w", err)
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("сервер вернул %d", resp.StatusCode)
+	}
+
+	filename := path.Base(resp.Request.URL.Path)
+	if filename == "." || filename == "/" || filename == "" {
+		return fmt.Errorf("не удалось определить имя файла из URL")
+	}
+
+	fullPath := path.Join(savePaths, filename)
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err = io.Copy(file, resp.Body); err != nil {
+		return err
+	}
+
+	fmt.Printf("Файл сохранён: %s\n", fullPath)
+
+	return nil
+}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -11,12 +51,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	savePaths := os.Args[1]
-	urls := os.Args[2:]
+	savePath := os.Args[1]
+	url := os.Args[2]
 
-	fmt.Printf("Директория для сохранения: %s\n", savePaths)
-	fmt.Println("URL для скачивания:")
-	for _, url := range urls {
-		fmt.Printf("- %s\n", url)
+	fmt.Printf("Скачивание: %s\n", url)
+
+	if err := downloadFile(url, savePath); err != nil {
+		fmt.Printf("Ошибка: %v\n", err)
+		os.Exit(1)
 	}
 }
